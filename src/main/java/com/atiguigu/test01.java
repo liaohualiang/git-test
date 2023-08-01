f16532cd03526ad897c03102c2fc8e6314d4b8ce
Lhl <Lhl@qq.com>
2023-08-01 14:03:21 +0800

    hot-fix commit

--- src/main/java/com/atiguigu/test01.java
+++ src/main/java/com/atiguigu/test01.java
@@ -4,5 +4,6 @@ public class test01 {
     public static void main(String[] args) {
         System.out.println("git first testS");
         System.out.println("git second test");
+        System.out.println("hot-fix test");
     }
 }
